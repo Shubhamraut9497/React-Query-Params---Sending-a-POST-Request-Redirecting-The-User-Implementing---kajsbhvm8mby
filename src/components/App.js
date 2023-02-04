584a3e33b5d1522b30322aaf77f45dc4eab7ab07
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-02-04 16:36:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   //Write your code here
-}
+  const [val, setVal] = useState({
+    id: "1",
+    name: "Robin",
+    designation: "scientist"
+  });
+  fetch("http://localhost:3000/?id=1&name=Robin&designation=scientist")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setVal((val) => {
+        return { ...val, data };
+      });
+    });
+  return (
+    <>
+      <div className="details">{`Employee ${val.id} named ${val.name} works as ${val.designation}`}</div>
+    </>
+  );
+};
 
 export default App;
